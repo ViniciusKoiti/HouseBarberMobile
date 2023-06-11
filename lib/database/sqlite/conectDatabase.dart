9303b10d20d1721ac400cbabb0220fee70b8ca44
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-06-11 15:34:53 -0300

    Ajustes sqls e Dao

--- lib/database/sqlite/conectDatabase.dart
+++ lib/database/sqlite/conectDatabase.dart
@@ -1,8 +1,6 @@
-
 import 'package:housebarber/database/sqlite/scripts/enumDatabase.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-
 
 class Conexao {
   static late Database _database;
@@ -10,18 +8,23 @@ class Conexao {
 
   static Future<Database> criar() async {
     if (_fechado) {
-      String path = join(await getDatabasesPath(),
-          'banco.db'); 
+      String path = join(await getDatabasesPath(), 'banco.db');
       _database = await openDatabase(
         path, // informando o caminho
         version: 1, // versão
         onCreate: (db, v) async {
-          await db.execute("CREATE TABLE person(id INTEGER PRIMARY KEY,name VARCHAR(100),telefone VARCHAR(100),imgUrl VARCHAR(100),cpfCpnj VARCHAR(100),cep VARCHAR(100))");
+          await createTables(db);
           insercoes.forEach(db.execute);
         },
       );
       _fechado = false;
     }
     return _database;
+  }
+
+  static Future<void> createTables(Database db) async {
+    for (var entry in tableSqlMap.entries) {
+      await db.execute(entry.value);
+    }
   }
 }
